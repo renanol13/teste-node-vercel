07358ff69f370ce9a660492f4cdee5b5b3a2771e
Renan <renan13.com@gmailcom>
2023-12-14 16:23:58 -0300

    instalando biblioteca cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const db = require('./dataBase/connection')
 db()
 
 app.use(express.json())
-
+app.use(cors())
 //routes
 const routes = require('./routers/persons')
 
