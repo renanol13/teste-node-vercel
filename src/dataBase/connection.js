5ef722b8936bf4f33863af0d3ad51dfa61725138
Renan <renan13.com@gmailcom>
2023-12-14 15:32:00 -0300

    mudando arquivo config 6

--- src/dataBase/connection.js
+++ src/dataBase/connection.js
@@ -6,6 +6,7 @@ const DATABASE_URL = process.env.DATABASE_URL
 const db = () => {
     try {
         mongoose.connect(DATABASE_URL)
+        console.log('logado com sucesso');
     } catch (error) {
         console.log(error);
     }
